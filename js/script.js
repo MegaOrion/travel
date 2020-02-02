fb62ecd0c1f0c681e5b25e14519b41c54e60f13d
MegaOrion <biryuk3333@gmail.com>
2020-02-02 21:59:06 +0200

    Modal

--- js/script.js
+++ js/script.js
@@ -78,4 +78,29 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     setClock('timer', deadline);
+
+    // Modal
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close'),
+        moreTabs = document.querySelectorAll('.description-btn');
+
+    more.addEventListener('click', showModal);
+    
+    moreTabs.forEach(function(item) {
+        item.addEventListener('click', showModal);
+    });    
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+
+    function showModal() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    }
 });
