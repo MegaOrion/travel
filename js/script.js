fd537052fb3e56fce4eb1b2b80e8792991a91b74
MegaOrion <biryuk3333@gmail.com>
2020-02-03 12:49:21 +0200

    SendForm

--- js/script.js
+++ js/script.js
@@ -105,5 +105,86 @@ window.addEventListener('DOMContentLoaded', () => {
         document.body.style.overflow = '';
     });
 
+    // Form
+
+    let message = {
+        loading: 'Загрузка...',
+        succes: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'Что-то пошло не так...'
+    };
+
+    let form = document.querySelector('.main-form'),    
+        formContact = document.getElementById('form'),        
+        statusMessage = document.createElement('div');
+        statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let input = form.getElementsByTagName('input');
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     
+        let formData = new FormData(form);
+
+        let obj = {};
+        formData.forEach((value, key) => {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', () => {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.succes;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        } 
+    });
+
+    formContact.addEventListener('submit', function(event) {
+        event.preventDefault();
+        formContact.appendChild(statusMessage);
+
+        let input = formContact.getElementsByTagName('input');
+        
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    
+        let formContactData = new FormData(formContact);
+
+        let obj = {};
+        formContactData.forEach((value, key) => {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', () => {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.succes;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    });
 });
